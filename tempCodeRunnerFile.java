0706617c645c5fdaadd453d27d297a19000978b7
KanwalB7 <148512493+KanwalB7@users.noreply.github.com>
2024-01-05 00:25:06 -0600

    --

--- tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-Implementor implementorA = new ConcreteImplementorA();
-            Abstraction abstractionA = new RefinedAbstraction(implementorA);
-            abstractionA.operation();
-            Implementor implementorB = new ConcreteImplementorB();
-            Abstraction abstractionB = new RefinedAbstraction(implementorB);
-            abstractionB.operation();
